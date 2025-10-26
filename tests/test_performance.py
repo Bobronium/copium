0ab3564b4eaf91b1fbb707e89eb770981b4d5a74
Bobronium <appkiller16@gmail.com>
2025-10-26 23:31:56 +0100

    Use benchmark directly

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -8,6 +8,5 @@ from datamodelzoo import CASES
     "case",
     (pytest.param(case, id=case.name) for case in CASES if "raises" not in case.name),
 )
-@pytest.mark.benchmark
-def test_individual_cases(case: Any, copy) -> None:
-    copy.deepcopy(case.obj)
+def test_individual_cases(case: Any, copy, benchmark) -> None:
+    benchmark(copy.deepcopy, case.obj)
