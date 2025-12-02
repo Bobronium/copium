bee7e158cb05d9170650f33eaf92795bb2cb9bac
Bobronium <appkiller16@gmail.com>
2025-12-02 11:01:55 +0100

    Add warmup runs

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -52,6 +52,25 @@ if not getattr(sys, "_is_gil_enabled", lambda: True)():
     python_version += "t"
 python_version += f"-{platform.machine()}"
 
+
+@pytest.mark.parametrize(
+    "case",
+    (pytest.param(case, id=case.name) for case in BASE_CASES),
+)
+@pytest.mark.parametrize("_python", [python_version])
+def test_individual_cases_warmup(case: Any, copy, _python, benchmark) -> None:
+    copy.deepcopy(case.obj)
+
+
+@pytest.mark.parametrize(
+    "case",
+    (pytest.param(case, id=case.name) for case in COMBINED_CASES),
+)
+@pytest.mark.parametrize("_python", [python_version])
+def test_combined_cases_warmup(case: Any, copy, _python, benchmark) -> None:
+    copy.deepcopy(case.obj)
+
+
 if python_version == "3.13-x86_64":
     # backwards compatibility with previous benchmarks runs
 
