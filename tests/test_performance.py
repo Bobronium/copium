f4f9a159c29a60cede6d9dc4712b0c615bcbedbe
Bobronium <appkiller16@gmail.com>
2025-12-02 10:32:31 +0100

    Run individual cases first

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -8,6 +8,7 @@ import sys
 from typing import Any
 
 import pytest
+
 from datamodelzoo import CASES
 from datamodelzoo import Case
 
@@ -56,27 +57,19 @@ if python_version == "3.13-x86_64":
 
     @pytest.mark.parametrize(
         "case",
-        (pytest.param(case, id=case.name) for case in COMBINED_CASES),
-    )
-    def test_combined_cases(case: Any, copy, benchmark) -> None:
-        benchmark(copy.deepcopy, case.obj)
-
-    @pytest.mark.parametrize(
-        "case",
         (pytest.param(case, id=case.name) for case in BASE_CASES),
     )
     def test_individual_cases(case: Any, copy, benchmark) -> None:
         benchmark(copy.deepcopy, case.obj)
 
-else:
-
     @pytest.mark.parametrize(
         "case",
         (pytest.param(case, id=case.name) for case in COMBINED_CASES),
     )
-    @pytest.mark.parametrize("_python", [python_version])
-    def test_combined_cases(case: Any, copy, benchmark, _python) -> None:
+    def test_combined_cases(case: Any, copy, benchmark) -> None:
         benchmark(copy.deepcopy, case.obj)
+
+else:
 
     @pytest.mark.parametrize(
         "case",
@@ -84,4 +77,12 @@ else:
     )
     @pytest.mark.parametrize("_python", [python_version])
     def test_individual_cases(case: Any, copy, benchmark, _python) -> None:
+        benchmark(copy.deepcopy, case.obj)
+
+    @pytest.mark.parametrize(
+        "case",
+        (pytest.param(case, id=case.name) for case in COMBINED_CASES),
+    )
+    @pytest.mark.parametrize("_python", [python_version])
+    def test_combined_cases(case: Any, copy, benchmark, _python) -> None:
         benchmark(copy.deepcopy, case.obj)
